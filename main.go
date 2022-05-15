a03dd36f73db2f60aeabd9233245196786f24feb
acepnurmansidik <acepnurmansidik@gmail.com>
2022-05-15 23:05:19 +0700

    Gorm get table data

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bwastartup/user"
 	"fmt"
 	"log"
 
@@ -10,11 +11,24 @@ import (
 
 func main() {
 	connection := "root:@tcp(127.0.0.1:3306)/crowfounding?charset=utf8mb4&parseTime=True&loc=Local"
-  	_, err := gorm.Open(mysql.Open(connection), &gorm.Config{})
+  	db, err := gorm.Open(mysql.Open(connection), &gorm.Config{})
 
+	//   jika ada error maka akan ditampilkan errornya
 	if err != nil {
 		log.Fatal(err.Error())
 	}
-
+	// Jika koneksi berhasil
 	fmt.Println("Connecting success")
+
+	// buat variabel utk menampung data
+	var users []user.User
+
+	db.Find(&users)
+
+	for _, user:= range users{
+		fmt.Println("==========")
+		fmt.Println(user.Name)
+		fmt.Println(user.Email)
+	}
+
 }
