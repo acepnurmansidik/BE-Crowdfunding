79e62a8a231e60eb232085232c3f21a2c1b56af2
acepnurmansidik <acepnurmansidik@gmail.com>
2022-05-16 00:33:46 +0700

    JSON result for web API

--- main.go
+++ main.go
@@ -2,14 +2,41 @@ package main
 
 import (
 	"bwastartup/user"
-	"fmt"
 	"log"
 
+	"github.com/gin-gonic/gin"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 func main() {
+	// connection := "root:@tcp(127.0.0.1:3306)/crowfounding?charset=utf8mb4&parseTime=True&loc=Local"
+  	// db, err := gorm.Open(mysql.Open(connection), &gorm.Config{})
+
+	// //   jika ada error maka akan ditampilkan errornya
+	// if err != nil {
+	// 	log.Fatal(err.Error())
+	// }
+	// // Jika koneksi berhasil
+	// fmt.Println("Connecting success")
+
+	// // buat variabel utk menampung data
+	// var users []user.User
+
+	// db.Find(&users)
+
+	// for _, user:= range users{
+	// 	fmt.Println("==========")
+	// 	fmt.Println(user.Name)
+	// 	fmt.Println(user.Email)
+	// }
+
+	router := gin.Default()
+	router.GET("/users", handler)
+	router.Run()
+}
+
+func handler(c *gin.Context){
 	connection := "root:@tcp(127.0.0.1:3306)/crowfounding?charset=utf8mb4&parseTime=True&loc=Local"
   	db, err := gorm.Open(mysql.Open(connection), &gorm.Config{})
 
@@ -17,18 +44,10 @@ func main() {
 	if err != nil {
 		log.Fatal(err.Error())
 	}
-	// Jika koneksi berhasil
-	fmt.Println("Connecting success")
 
-	// buat variabel utk menampung data
 	var users []user.User
-
 	db.Find(&users)
 
-	for _, user:= range users{
-		fmt.Println("==========")
-		fmt.Println(user.Name)
-		fmt.Println(user.Email)
-	}
-
+	// ubah ke dalam JSON
+	c.JSON(200, users)
 }
