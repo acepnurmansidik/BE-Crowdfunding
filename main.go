62cf52fe1aa47344e260507440f9e8907287f1ba
acepnurmansidik <acepnurmansidik@gmail.com>
2022-05-28 14:47:08 +0700

    Membuat repository

--- main.go
+++ main.go
@@ -4,39 +4,11 @@ import (
 	"bwastartup/user"
 	"log"
 
-	"github.com/gin-gonic/gin"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 func main() {
-	// connection := "root:@tcp(127.0.0.1:3306)/crowfounding?charset=utf8mb4&parseTime=True&loc=Local"
-  	// db, err := gorm.Open(mysql.Open(connection), &gorm.Config{})
-
-	// //   jika ada error maka akan ditampilkan errornya
-	// if err != nil {
-	// 	log.Fatal(err.Error())
-	// }
-	// // Jika koneksi berhasil
-	// fmt.Println("Connecting success")
-
-	// // buat variabel utk menampung data
-	// var users []user.User
-
-	// db.Find(&users)
-
-	// for _, user:= range users{
-	// 	fmt.Println("==========")
-	// 	fmt.Println(user.Name)
-	// 	fmt.Println(user.Email)
-	// }
-
-	router := gin.Default()
-	router.GET("/users", handler)
-	router.Run()
-}
-
-func handler(c *gin.Context){
 	connection := "root:@tcp(127.0.0.1:3306)/crowfounding?charset=utf8mb4&parseTime=True&loc=Local"
   	db, err := gorm.Open(mysql.Open(connection), &gorm.Config{})
 
@@ -45,9 +17,11 @@ func handler(c *gin.Context){
 		log.Fatal(err.Error())
 	}
 
-	var users []user.User
-	db.Find(&users)
+	userRepository := user.NewRepository(db)
+	user := user.User{
+		Name: "norman",
+	}
 
-	// ubah ke dalam JSON
-	c.JSON(200, users)
+	userRepository.Save(user)
+	
 }
