5f3f7b1aec6f3017d9677c14c89422364fad120c
acepnurmansidik <acepnurmansidik@gmail.com>
2022-06-01 08:04:13 +0700

    JWT | validasi token

--- auth/service.go
+++ auth/service.go
@@ -1,9 +1,14 @@
 package auth
 
-import "github.com/dgrijalva/jwt-go"
+import (
+	"errors"
+
+	"github.com/dgrijalva/jwt-go"
+)
 
 type Service interface {
 	GenerateToken(userID int) (string, error)
+	ValidateToken(encodeToken string) (*jwt.Token, error)
 }
 
 type jwtService struct{
@@ -27,10 +32,29 @@ func (s *jwtService) GenerateToken(userID int) (string, error) {
 
 	// buat verify signature/ secret key
 	signedToken, err := token.SignedString(SECRET_KEY)
-	
+
 	if err != nil {
 		return signedToken, err
 	}
 	
 	return signedToken, nil
+}
+
+func (s *jwtService) ValidateToken(encodeToken string) (*jwt.Token, error){
+	// parse encodeToken
+	token, err := jwt.Parse(encodeToken, func(token *jwt.Token) (interface{}, error){
+		// cek apakah algoritma yang digunakan sama tidak dengan yg dikirim
+		_, ok := token.Method.(*jwt.SigningMethodHMAC)
+
+		if !ok {
+			return nil, errors.New("Invalid token")
+		}
+		return []byte(SECRET_KEY), nil
+	})
+
+	if err != nil{
+		return token, err
+	}
+
+	return token, nil
 }
