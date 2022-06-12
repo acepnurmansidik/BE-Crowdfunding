1c3e44b1d68eea2e6e9688d0e51ff437229d24a4
acepnurmansidik <acepnurmansidik@gmail.com>
2022-06-12 14:40:38 +0700

    Transaction | repository (Get Campaigns by id)

--- transaction/repository.go
+++ transaction/repository.go
@@ -1,1 +1,25 @@
 package transaction
+
+import "gorm.io/gorm"
+
+type Repository interface{
+	GetCampaignByID(CampaignID int) ([]Transaction, error)
+}
+
+type repository struct{
+	db *gorm.DB
+}
+
+func NewRepository (db *gorm.DB) *repository {
+	return &repository{db}
+}
+
+func (r *repository) GetCampaignByID(CampaignID int) ([]Transaction, error){
+	var transactions []Transaction
+	err := r.db.Preload("User").Where("campaign_id = ?", CampaignID).Find(&transactions).Error
+	if err != nil {
+		return transactions, err
+	}
+
+	return transactions, nil
+}
