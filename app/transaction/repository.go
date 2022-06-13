f8c8c09ebbefd8b5ab56a14bf4ec899880a86f2c
acepnurmansidik <acepnurmansidik@gmail.com>
2022-06-14 06:41:44 +0700

    User Transaction API | repository

--- app/transaction/repository.go
+++ app/transaction/repository.go
@@ -4,6 +4,7 @@ import "gorm.io/gorm"
 
 type Repository interface{
 	GetCampaignByID(CampaignID int) ([]Transaction, error)
+	GetUserTransaction(userID int) ([]Transaction, error)
 }
 
 type repository struct{
@@ -19,6 +20,18 @@ func (r *repository) GetCampaignByID(CampaignID int) ([]Transaction, error){
 	err := r.db.Preload("User").Where("campaign_id = ?", CampaignID).Order("id desc").Find(&transactions).Error
 	if err != nil {
 		return transactions, err
+	}
+
+	return transactions, nil
+}
+
+func (r *repository) GetUserTransaction(userID int) ([]Transaction, error){
+	var transactions []Transaction
+
+	err := r.db.Preload("User").Where("user_id = ?", userID).Order("id desc").Find(&transactions).Error
+
+	if err != nil {
+		return []Transaction{}, err
 	}
 
 	return transactions, nil
