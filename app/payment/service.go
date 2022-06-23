62a7e90012ee7fe10a3633a1c442e03e18ba5a5b
acepnurmansidik <acepnurmansidik@gmail.com>
2022-06-24 06:04:38 +0700

    fix code not import cycle and added alias

--- app/payment/service.go
+++ app/payment/service.go
@@ -1,24 +1,23 @@
 package payment
 
 import (
-	"bwastartup/app/transaction"
 	"bwastartup/app/user"
 	"strconv"
 
-	"github.com/veritrans/go-midtrans"
+	midtrans "github.com/veritrans/go-midtrans"
 )
 
 type service struct{}
 
 type Service interface{
-	GetPaymentURL(transaction transaction.Transaction, user user.User) (string, error)
+	GetPaymentURL(transaction Transaction, user user.User) (string, error)
 }
 
 func NewService() *service{
 	return &service{}
 }
 
-func (s *service) GetPaymentURL(transaction transaction.Transaction, user user.User) (string, error){
+func (s *service) GetPaymentURL(transaction Transaction, user user.User) (string, error){
 	midclient := midtrans.NewClient()
 	midclient.ServerKey = "YOUR-VT-SERVER-KEY"
     midclient.ClientKey = "YOUR-VT-CLIENT-KEY"
